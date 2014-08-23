3b429229e94a5ec7a9cffc00de6aebae8cf33b73
danigattoni <danigattoni@gmail.com>
2014-08-23 12:49:53 -0400

    twitter stream example 3

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,29 +6,29 @@ class WelcomeController < ApplicationController
     def index
       @pins = Pin.all
 
-      client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = "I9ikHMfYPqkGK8fkglKNACfNU"
-      config.consumer_secret     = "pQQ7rCkMin7GvQdsaFvPtahthIjdiUwQpIkx5miZmh0wJU1Uzz"
-      config.access_token        = "445571280-j2m2vwaOb4mDb1qBXUvmT6oqlh0Topiixh6vfX3u"
-      config.access_token_secret = "9w5GsOzAitaDl13KGFFFvu7oLmT1xQpJpEAALFzDWdmbG" 
-      end
+      # client = Twitter::REST::Client.new do |config|
+      # config.consumer_key        = "I9ikHMfYPqkGK8fkglKNACfNU"
+      # config.consumer_secret     = "pQQ7rCkMin7GvQdsaFvPtahthIjdiUwQpIkx5miZmh0wJU1Uzz"
+      # config.access_token        = "445571280-j2m2vwaOb4mDb1qBXUvmT6oqlh0Topiixh6vfX3u"
+      # config.access_token_secret = "9w5GsOzAitaDl13KGFFFvu7oLmT1xQpJpEAALFzDWdmbG" 
+      # end
 
 
     end
 
 
 
-    def client.get_all_tweets(user)
+    # def client.get_all_tweets(user)
 
 
-      options = {:count => 8, :include_rts => true}
-      user_timeline(user, options)
-      end
+    #   options = {:count => 8, :include_rts => true}
+    #   user_timeline(user, options)
+    #   end
 
 
-      @tweet_news = client.get_all_tweets("danigattoni_")
+    #   @tweet_news = client.get_all_tweets("danigattoni_")
 
-    end
+    # end
 
 
 
