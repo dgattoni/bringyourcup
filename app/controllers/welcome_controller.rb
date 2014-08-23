d62fc74cd0cb560c202ed476d6e441a991c0211c
danigattoni <danigattoni@gmail.com>
2014-08-23 13:08:53 -0400

    twitter stream example 6

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -21,36 +21,13 @@ class WelcomeController < ApplicationController
     config.auth_method        = :oauth
     end
 
-    EM.run do
-
-    client = TweetStream::Client.new
-
-    client.on_error do |error|
-    puts error
+    # This will pull a sample of all tweets based on
+    # your Twitter account's Streaming API role.
+    TweetStream::Client.new.sample do |status|
+    # The status object is a special Hash with
+    # method access to its keys.
+    puts "#{status.text}"
     end
-
-    client.sitestream([445571280], :followings => true) do |status|
-    puts status.inspect
-    end
-
-    EM::Timer.new(60) do
-    client.control.add_user(user_id_to_add)
-    client.control.info { |i| puts i.inspect }
-    end
-
-    EM::Timer.new(75) do
-    client.control.friends_ids(user_id) do |friends|
-    puts friends.inspect
-    end
-    end
-
-    EM::Timer.new(90) do
-    client.control.remove_user(user_id_to_remove)
-    client.control.info { |i| puts i.inspect }
-    end
-
-    end
-
 
 
 
