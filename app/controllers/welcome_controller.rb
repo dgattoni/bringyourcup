126dc5b960db422d00459557aa0a5fde2cab25e0
danigattoni <danigattoni@gmail.com>
2014-08-24 11:22:20 -0400

    twitter stream example 16

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -12,6 +12,7 @@ class WelcomeController < ApplicationController
       # config.access_token_secret = "9w5GsOzAitaDl13KGFFFvu7oLmT1xQpJpEAALFzDWdmbG" 
       # end
       #TweetStream::Client.new(:username => 'danigattoni_', :password => 'aznar2012')
+
       # TweetStream.configure do |config|
       # config.consumer_key       = 'I9ikHMfYPqkGK8fkglKNACfNU'
       # config.consumer_secret    = 'pQQ7rCkMin7GvQdsaFvPtahthIjdiUwQpIkx5miZmh0wJU1Uzz'
@@ -26,17 +27,20 @@ class WelcomeController < ApplicationController
 
    #  if params[:q]
       client = Twitter::REST::Client.new do |config|
-      config.consumer_key       = 'I9ikHMfYPqkGK8fkglKNACfNU'
-      config.consumer_secret    = 'pQQ7rCkMin7GvQdsaFvPtahthIjdiUwQpIkx5miZmh0wJU1Uzz'
-      config.oauth_token        = '445571280-j2m2vwaOb4mDb1qBXUvmT6oqlh0Topiixh6vfX3u'
-      config.oauth_token_secret = '9w5GsOzAitaDl13KGFFFvu7oLmT1xQpJpEAALFzDWdmbG'
-      config.auth_method        = :oauth
+        config.consumer_key        = "I9ikHMfYPqkGK8fkglKNACfNU"
+        config.consumer_secret     = "pQQ7rCkMin7GvQdsaFvPtahthIjdiUwQpIkx5miZmh0wJU1Uzz"
+        config.access_token        = "445571280-j2m2vwaOb4mDb1qBXUvmT6oqlh0Topiixh6vfX3u"
+        config.access_token_secret = "9w5GsOzAitaDl13KGFFFvu7oLmT1xQpJpEAALFzDWdmbG" 
       end
 
      # topics = ["rock"]
-     # @search = client.search(topics.join(","))
-   
-      @search = client.search :q => 'Pearl Jam', :count => 8
+      
+      @search = client.search("rock", :count => 4, :result_type => "recent")
+
+      #client.search("to:justinbieber marry me", :result_type => "recent").take(3).each do |tweet|
+      
+
+      #@search = client.search :q => 'Pearl Jam', :count => 8
     
     #end def index
     end 
