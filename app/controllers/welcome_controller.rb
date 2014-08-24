52f1f5b0c630c62fbd7c918481601d1b20ef6a5a
danigattoni <danigattoni@gmail.com>
2014-08-24 11:12:34 -0400

    twitter stream example 14

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -11,20 +11,32 @@ class WelcomeController < ApplicationController
       # config.access_token        = "445571280-j2m2vwaOb4mDb1qBXUvmT6oqlh0Topiixh6vfX3u"
       # config.access_token_secret = "9w5GsOzAitaDl13KGFFFvu7oLmT1xQpJpEAALFzDWdmbG" 
       # end
-   # TweetStream::Client.new(:username => 'danigattoni_', :password => 'aznar2012')
+      #TweetStream::Client.new(:username => 'danigattoni_', :password => 'aznar2012')
+      # TweetStream.configure do |config|
+      # config.consumer_key       = 'I9ikHMfYPqkGK8fkglKNACfNU'
+      # config.consumer_secret    = 'pQQ7rCkMin7GvQdsaFvPtahthIjdiUwQpIkx5miZmh0wJU1Uzz'
+      # config.oauth_token        = '445571280-j2m2vwaOb4mDb1qBXUvmT6oqlh0Topiixh6vfX3u'
+      # config.oauth_token_secret = '9w5GsOzAitaDl13KGFFFvu7oLmT1xQpJpEAALFzDWdmbG'
+      # config.auth_method        = :oauth
+      # end
+      #      TweetStream::Client.new('user','pass').track('fail') 
+      #@jazztweets = TweetStream::Client.new.track('feedback')
+      #@client = TweetStream::Client.new('danigattoni_','aznar2012')
+      #  
 
-    TweetStream.configure do |config|
-    config.consumer_key       = 'I9ikHMfYPqkGK8fkglKNACfNU'
-    config.consumer_secret    = 'pQQ7rCkMin7GvQdsaFvPtahthIjdiUwQpIkx5miZmh0wJU1Uzz'
-    config.oauth_token        = '445571280-j2m2vwaOb4mDb1qBXUvmT6oqlh0Topiixh6vfX3u'
-    config.oauth_token_secret = '9w5GsOzAitaDl13KGFFFvu7oLmT1xQpJpEAALFzDWdmbG'
-    config.auth_method        = :oauth
-    end
-#      TweetStream::Client.new('user','pass').track('fail') 
+   #  if params[:q]
+      client = Twitter::REST::Client.new do |config|
+      config.consumer_key       = 'I9ikHMfYPqkGK8fkglKNACfNU'
+      config.consumer_secret    = 'pQQ7rCkMin7GvQdsaFvPtahthIjdiUwQpIkx5miZmh0wJU1Uzz'
+      config.oauth_token        = '445571280-j2m2vwaOb4mDb1qBXUvmT6oqlh0Topiixh6vfX3u'
+      config.oauth_token_secret = '9w5GsOzAitaDl13KGFFFvu7oLmT1xQpJpEAALFzDWdmbG'
+      end
 
-
-    @jazztweets = TweetStream::Client.new.('danigattoni_','aznar2012').track('feedback')
-
+     # topics = ["rock"]
+     # @search = client.search(topics.join(","))
+   
+      @search = client.search :q => 'Pearl Jam', :count => 8
+    
     #end def index
     end 
 
@@ -34,7 +46,7 @@ class WelcomeController < ApplicationController
     # def client.get_all_tweets(user)
 
 
-    #   options = {:count => 8, :include_rts => true}
+     # options = {:count => 8, :include_rts => true}
     #   user_timeline(user, options)
     #   end
 
