c2d4d98f82a05d448a8f340e40269e906bc1e7bc
danigattoni <danigattoni@gmail.com>
2014-08-24 11:14:38 -0400

    twitter stream example 15

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -30,6 +30,7 @@ class WelcomeController < ApplicationController
       config.consumer_secret    = 'pQQ7rCkMin7GvQdsaFvPtahthIjdiUwQpIkx5miZmh0wJU1Uzz'
       config.oauth_token        = '445571280-j2m2vwaOb4mDb1qBXUvmT6oqlh0Topiixh6vfX3u'
       config.oauth_token_secret = '9w5GsOzAitaDl13KGFFFvu7oLmT1xQpJpEAALFzDWdmbG'
+      config.auth_method        = :oauth
       end
 
      # topics = ["rock"]
