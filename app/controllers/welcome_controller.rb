c5904248a2fde4521e932cc1bce960bc3b5de8cc
danigattoni <danigattoni@gmail.com>
2014-08-30 16:02:32 -0400

    twitter 30agosto 4

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,13 +6,12 @@ class WelcomeController < ApplicationController
       @pins = Pin.all
  
   
-      
-      # client = Twitter::REST::Client.new do |config|
-      #   config.consumer_key        = "I9ikHMfYPqkGK8fkglKNACfNU"
-      #   config.consumer_secret     = "pQQ7rCkMin7GvQdsaFvPtahthIjdiUwQpIkx5miZmh0wJU1Uzz"
-      #   config.access_token        = "445571280-j2m2vwaOb4mDb1qBXUvmT6oqlh0Topiixh6vfX3u"
-      #   config.access_token_secret = "9w5GsOzAitaDl13KGFFFvu7oLmT1xQpJpEAALFzDWdmbG" 
-      # end
+       client = Twitter::REST::Client.new do |config|
+            config.consumer_key        = "I9ikHMfYPqkGK8fkglKNACfNU"
+           config.consumer_secret     = "pQQ7rCkMin7GvQdsaFvPtahthIjdiUwQpIkx5miZmh0wJU1Uzz"
+           config.access_token        = "445571280-j2m2vwaOb4mDb1qBXUvmT6oqlh0Topiixh6vfX3u"
+          config.access_token_secret = "9w5GsOzAitaDl13KGFFFvu7oLmT1xQpJpEAALFzDWdmbG" 
+      end
 
     #----------------------------------------------------------------------------------
         #EJEMPLO OK 1:
--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,15 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
+       client = Twitter::REST::Client.new do |config|
+            config.consumer_key        = "I9ikHMfYPqkGK8fkglKNACfNU"
+           config.consumer_secret     = "pQQ7rCkMin7GvQdsaFvPtahthIjdiUwQpIkx5miZmh0wJU1Uzz"
+           config.access_token        = "445571280-j2m2vwaOb4mDb1qBXUvmT6oqlh0Topiixh6vfX3u"
+          config.access_token_secret = "9w5GsOzAitaDl13KGFFFvu7oLmT1xQpJpEAALFzDWdmbG" 
+      end
+      
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
