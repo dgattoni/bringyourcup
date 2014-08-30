576b43bf98c403709ad2b42c60c1eca846f8ec1c
danigattoni <danigattoni@gmail.com>
2014-08-29 23:13:27 -0400

    twitter test 7

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -39,7 +39,7 @@ class WelcomeController < ApplicationController
 
 
   #---EJEMPLO OK 5: -------------------------------------------------------------------------------
-       username = 'danigattoni_'
+       username = 'metrodesantiago'
        options = {:count => 6, :include_rts => false, :exclude_replies => true}
        @search = client.user_timeline(username, options)
     #---EJEMPLO OK 5: -------------------------------------------------------------------------------
