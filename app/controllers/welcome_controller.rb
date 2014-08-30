fb32e1f27a3390ed338c589afb00ee7f4399fbb3
danigattoni <danigattoni@gmail.com>
2014-08-30 18:34:52 -0400

    twitter 30agosto 29

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -37,67 +37,25 @@ class WelcomeController < ApplicationController
        options = { :until=>'2014-08-24', :count => 10, :include_rts => 0, :exclude_replies => 1, :retweeted => 0, :result_type=> 'recent', :geo => 1 }
        @search = $client.user_timeline(username, options)
       
-       $mifoto = $client.user('danigattoni_').profile_image_url("normal")
+
 
     #---EJEMPLO OK 5: -------------------------------------------------------------------------------
 
 
 
-#---EJEMPO OK 6: ----------------------------------
+
+#---EJEMPO OK 6: --------------------------------------------------------------------------
      
-
-
-       
-     # topics = [params[:q]]
-
-
-     # lati =params[:lati]+","
-     # longi=params[:longi]+","
-     # millas=params[:millas]
-     # coord = lati+longi+millas
-      #coord ="-33,-70,1000mi"
-
-      #"-33,-70,100mi"
       
-     # @search = client.search(topics.join(","), :lang => "es", :geocode => "-33.4691199, -70.641997, 50km").take(5).collect
-      
-     # @search = client.search(topics.join(","), :locale => "cl" , :geocode => "-33,-70,100mi").take(5).collect
-      
-      #@search = client.search(topics.join(","), :geocode => coord, :exclude_replies => false).take(5).collect
+       $mifoto = $client.user('danigattoni_').profile_image_url("normal")
 
+#----------------------------------------------------------------------------------------------
 
+  
 
-          #<% if params[:q]  %>
-          # <form>
-          #    Ingresa una palabra clave para buscar:
-
-          #    <span style="color:purple">Tema: </span><input name="q"><br>
-          #    <span style="color:red">Latitud: (ej: -33) </span><input name="lati"> <br>
-          #    <span style="color:orange">Longitud: (ej: -70) </span><input name="longi"><br>
-          #    <span style="color:green">Millas a la redonda: (ej: 100mi)</span><input name="millas"> <br>
-
-          #    <input type="submit">
-          #    </form>
-          #
-          #               <p>Topico buscado:  <strong><%= params[:q]  %> </strong></p>
-          #              <% @search.each do |s|%>
-          #             <p> <%= s.text %></p>
-          #            <% end %>
-
-          #   <% end %>
-
-
-#--------------------------------------------
-
-     # topics = ["rock"]
-      #client.search("#ruby -rt", :lang => "ja").first.text 
-
-
-      
-
-      #client.search("to:justinbieber marry me", :result_type => "recent").take(3).each do |tweet|
-      
-
+    # topics = ["rock"]
+    #client.search("#ruby -rt", :lang => "ja").first.text 
+    #client.search("to:justinbieber marry me", :result_type => "recent").take(3).each do |tweet|
     #end def index
     end 
 
@@ -105,18 +63,33 @@ class WelcomeController < ApplicationController
 
 
     # def client.get_all_tweets(user)
-
-
      # options = {:count => 8, :include_rts => true}
     #   user_timeline(user, options)
     #   end
-
-
     #   @tweet_news = client.get_all_tweets("danigattoni_")
-
     # end
 
+    def buscar_tweet
 
+
+       
+     topics = [params[:q]]
+
+
+     lati =params[:lati]+","
+     longi=params[:longi]+","
+     millas=params[:millas]
+     coord = lati+longi+millas
+     # coord ="-33,-70,1000mi"
+
+     
+      
+    #@search = client.search(topics.join(","), :lang => "es", :geocode => "-33.4691199, -70.641997, 50km").take(5).collect
+    #@search = client.search(topics.join(","), :locale => "cl" , :geocode => "-33,-70,100mi").take(5).collect
+    @search = $client.search(topics.join(","), :geocode => coord, :exclude_replies => 0, :result_type=>"recent").take(5).collect
+
+
+    end
 
 
     def nosotros
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get 'welcome/nosotros'
   get 'welcome/descubre'
   get 'welcome/fullscreen'
+  get 'welcome/buscar_tweets'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
