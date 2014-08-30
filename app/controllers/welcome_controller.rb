179f725d15354396d4a157a4d7c03c82c74108fd
danigattoni <danigattoni@gmail.com>
2014-08-30 19:11:06 -0400

    twitter 30agosto 40

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -73,7 +73,7 @@ class WelcomeController < ApplicationController
 
     #$mifoto = $client.user('danigattoni_').profile_image_url("normal")
        
-     topic = "jazz" 
+     topic = "concierto" 
      #lati =params[:lati]
      #longi=params[:longi]
      #millas=params[:millas]
@@ -85,7 +85,7 @@ class WelcomeController < ApplicationController
     #@search = client.search(topics.join(","), :lang => "es", :geocode => "-33.4691199, -70.641997, 50km").take(5).collect
     #@search = client.search(topics.join(","), :locale => "cl" , :geocode => "-33,-70,100mi").take(5).collect
     #@search = $client.search(topic, :geocode => coord, :exclude_replies => 0, :result_type=>"recent").take(5).collect
-    @search = $client.search(topic, :geocode => "-33,-70,10000mi", :exclude_replies => 0, :result_type=>"recent").take(5).collect
+    @search = $client.search(topic,:exclude_replies => 0, :result_type=>"recent").take(5).collect
 
 
     end
