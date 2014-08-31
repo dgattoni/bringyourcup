5d166258a384ac2b3e99dfcac0ccc57d7b0db402
danigattoni <danigattoni@gmail.com>
2014-08-30 20:13:43 -0400

    twitter 30agosto 49

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -70,22 +70,24 @@ class WelcomeController < ApplicationController
     # end
 
     def buscar_tweets
+      @pins = Pin.all
+      #$mifoto = $client.user('danigattoni_').profile_image_url("normal")
+      topics =  "emprendimiento"
 
-    #$mifoto = $client.user('danigattoni_').profile_image_url("normal")
-    topics =  "jazz,rock"
-     
-     #lati =params[:lati]
-     #longi=params[:longi]
-     #millas=params[:millas]
-     #coord = lati+","+longi+","+millas
-     # coord ="-33,-70,1000mi"
+      #lati =params[:lati]
+      #longi=params[:longi]
+      #millas=params[:millas]
+      #coord = lati+","+longi+","+millas
+      # coord ="-33,-70,1000mi"
 
-        #@search = client.search(topics.join(","), :lang => "es", :geocode => "-33.4691199, -70.641997, 50km").take(5).collect
-        #@search = client.search(topics.join(","), :locale => "cl" , :geocode => "-33,-70,100mi").take(5).collect
-        #@search = $client.search(topic, :geocode => coord, :exclude_replies => 0, :result_type=>"recent").take(5).collect
-   
-          @busca = $client.search(topics.join(","), :count=>5, :lang => "en", :exclude_replies => 0, :result_type=>"popular")
-         
+      #@search = client.search(topics.join(","), :lang => "es", :geocode => "-33.4691199, -70.641997, 50km").take(5).collect
+      #@search = client.search(topics.join(","), :locale => "cl" , :geocode => "-33,-70,100mi").take(5).collect
+      #@search = $client.search(topic, :geocode => coord, :exclude_replies => 0, :result_type=>"recent").take(5).collect
+
+      #@busca = $client.search(topics.join(","), :count=>5, :lang => "en", :exclude_replies => 0, :result_type=>"popular")
+
+      @busca = $client.search(topics.join(","))
+
 
     end
 
