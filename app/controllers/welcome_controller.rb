c435710b7baedae3eccb93a4b9aecf4fe5a92e98
danigattoni <danigattoni@gmail.com>
2014-08-30 21:17:19 -0400

    twitter 30agosto 64

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -91,10 +91,13 @@ class WelcomeController < ApplicationController
      #      topics = ["brmc"]
 
 
-        if !params[:q].nil?  
-        topics =[params[:q]]    
-        @search = $client.search(topics.join(","),:result_type => "recent", :include_rts => 0,:exclude_replies => 0,:lang=>"es",:count=>5).take(5).collect
+        if params[:q].nil?  
+          @search=""  
+        else
+          topics =[params[:q]]    
+          @search = $client.search(topics.join(","),:result_type => "recent", :include_rts => 0,:exclude_replies => 0,:lang=>"es",:count=>5).take(5).collect
         end
+
 
     end
 
