6fc3f170733ff5a0ba8ec1226de19aebc8ec520d
danigattoni <danigattoni@gmail.com>
2014-08-30 22:04:59 -0400

    twitter 30agosto 75

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -94,11 +94,11 @@ class WelcomeController < ApplicationController
        if request.GET.include? "q"
 
         topics =[params[:q]]
-      else
-        topics =[""]
+         @search = $client.search(topics.join(","),:result_type => "recent", :include_rts => 0,:exclude_replies => 0,:lang=>"es",:count=>5).take(5).collect
+
+      
       end
 
-       @search = $client.search(topics.join(","),:result_type => "recent", :include_rts => 0,:exclude_replies => 0,:lang=>"es",:count=>5).take(5).collect
 
   
 
