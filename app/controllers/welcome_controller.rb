89cb4b27a302e880af891fef17059da96f1396df
danigattoni <danigattoni@gmail.com>
2014-09-06 17:30:36 -0400

    twitter 6septiembre 17

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -22,7 +22,9 @@ class WelcomeController < ApplicationController
               # accuracy 
 
 
-            @search = $client.search("santiago",:count=>5,:geocode=>"-33.4691199,-70.641997,150km").take(3).collect
+           #@search = $client.search("santiago",:count=>5,:geocode=>"-33.4691199,-70.641997,150km").take(3).collect
+            @search = $client.search("santiago", :lang => "es", :geocode=>"-33,-70,150km").take(3).collect
+
         #-----------------------------------------------------------------------------
      
     #---EJEMPLO OK 2: -------------------------------------------------------------------------------
