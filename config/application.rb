9157395343eb4688cac61db23deaf7fc09cf710c
danigattoni <danigattoni@gmail.com>
2014-09-10 17:50:53 -0300

    twitter 10 sept 02

--- config/application.rb
+++ config/application.rb
@@ -20,9 +20,9 @@ module Pinterest
 
       $client = Twitter::REST::Client.new do |config|
             
-          config.consumer_key      = ENV['TWITTER_API_KEY']
-          config.consumer_secret    = ENV['TWITTER_API_SECRET']
-          config.access_token       = ENV['TWITTER_ACCESS_TOKEN']
+          config.consumer_key        = ENV['TWITTER_API_KEY']
+          config.consumer_secret     = ENV['TWITTER_API_SECRET']
+          config.access_token        = ENV['TWITTER_ACCESS_TOKEN']
           config.access_token_secret = ENV['TWITTER_ACCESS_TOKEN_SECRET']
       end
       
